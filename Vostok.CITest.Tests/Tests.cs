e2fec4ac159eea94650865ff872a23fc07f9e8a6
iloktionov <iloktionov@skbkontur.ru>
2018-07-23 20:23:39 +0500

    Not important.

---
+++ Vostok.CITest.Tests/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Vostok.CITest.Tests
+{
+    [TestFixture]
+    internal class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+    }
+}
